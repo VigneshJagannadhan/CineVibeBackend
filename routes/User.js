08bcfc56fb965bc3b0e484914da581a6f3356beb
vignesh_jagan <vigneshjagannadhan1998@gmail.com>
2024-08-01 09:19:33 +0530

    Bug fix

--- routes/User.js
+++ routes/User.js
@@ -1,7 +1,7 @@
 // routes/user.js
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
