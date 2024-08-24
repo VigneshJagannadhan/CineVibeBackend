64e038b0e0df63868f03a7e3dedb21ae70015e60
vignesh_jagan <vigneshjagannadhan1998@gmail.com>
2024-08-25 01:20:55 +0530

    Bug fixes

--- server.js
+++ server.js
@@ -2,35 +2,33 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
 const userRoutes = require('./routes/user');
 const authRoutes = require('./routes/auth');
-const movieRoutes = require('./routes/movie'); // Assuming you have this file for movie routes
+const movieRoutes = require('./routes/movie');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
 
-// Use the CORS middleware
 app.use(cors());
 
 app.use(bodyParser.json());
 
 app.use('/users', userRoutes);
 app.use('/auth', authRoutes);
-app.use('/movies', movieRoutes); // Assuming you have movie routes
+app.use('/movies', movieRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Movie Review App Backend');
+  res.send('Cinevibe App Backend');
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-const mongoURI = process.env.MONGODB_URI || 'your-default-mongodb-uri';
+const mongoURI = process.env.MONGODB_URI;
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected...'))
+}).then(() => console.log('MongoDB connected...'))
 .catch(err => console.log('MongoDB connection error:', err));
